0886f78f76c1fdf0abb313e05aa447cbf7a3ba74
Ubuntu <ubuntu@ip-172-31-15-233.us-west-1.compute.internal>
2023-02-10 16:52:53 +0000

    updated code - data5500 spring2023

--- week3_bigOnotation/bubble_sort.py
+++ week3_bigOnotation/bubble_sort.py
@@ -3,9 +3,12 @@ lst = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9]
 # n = number of items in the list i.e. 13
 
 for j in range(len(lst)-1):
+    ct = 0
     for i in range(len(lst)-1):
         #compare two elements and swap if elemnt 1 is greater
         if lst[i] > lst[i+1]:
             lst[i], lst[i+1] = lst[i+1], lst[i]
-
+            ct += 1
+    if ct == 0:
+        break
 print("lst: ", lst)
